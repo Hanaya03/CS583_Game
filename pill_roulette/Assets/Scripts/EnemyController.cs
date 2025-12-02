902c3319d908ff0594f20a938547c6c40a5ecdfb
Hanaya03 <ha4863519@gmail.com>
2025-12-02 14:36:20 -0800

    enemy animations integrated to gameplay

--- pill_roulette/Assets/Scripts/EnemyController.cs
+++ pill_roulette/Assets/Scripts/EnemyController.cs
@@ -11,7 +11,7 @@ public class EnemyController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _animator.SetBool("Idle", true);
     }
 
     // Update is called once per frame
@@ -22,6 +22,7 @@ public class EnemyController : MonoBehaviour
 
     public IEnumerator TakeTurn(List<Cup> cups)
     {
+        TransitionToReach();
         // Collect remaining sushi (still active)
         List<Cup> remaining = new List<Cup>();
         foreach (var c in cups)
@@ -37,11 +38,19 @@ public class EnemyController : MonoBehaviour
 
         // NPC random pick among remaining
         Cup choice = remaining[Random.Range(0, remaining.Count)];
+        while(!choice.SushiUnderCup.IsPoisoned)
+        {
+            choice = remaining[Random.Range(0, remaining.Count)];
+        }
 
         float yStart  = _gameManager.STARTLOCALY[choice];
         float yReveal = yStart - _gameManager.DROPOFFSET;
 
         yield return choice.StartCoroutine(choice.MoveLocalY(yStart, yReveal, _gameManager.RAISEDURATION, _gameManager.MOVECURVE));
+        yield return new WaitForSeconds(.42f);
+
+        TransitionToEat();
+        yield return new WaitForSeconds(.5f);
 
         bool poisoned = choice.SushiUnderCup.IsPoisoned;
         choice.SushiUnderCup.Eat();
@@ -54,14 +63,58 @@ public class EnemyController : MonoBehaviour
             if (_health <= 0)
             {
                 _gameManager.GameOver = true;
+                TransitionToDying();
+                yield return new WaitForSeconds(.5f);
+                TransitionToDead();
                 Debug.Log("<color=cyan>NPC died. YOU WIN!</color>");
                 yield break;
             }
 
+            TransitionToIdle();
             // Round ends because poison was eaten → start next round
             yield return new WaitForSeconds(0.6f);
             yield return _gameManager.SetupNewRound();
             yield break;
         }
+        TransitionToIdle();
+    }
+
+    private void TransitionToIdle()
+    {
+        ResetAllBools();
+        _animator.SetBool("Idle", true);
+    }
+
+    private void TransitionToReach()
+    {
+        ResetAllBools();
+        _animator.SetBool("Reaching", true);
+    }
+
+    private void TransitionToEat()
+    {
+        ResetAllBools();
+        _animator.SetBool("Eating", true);
+    }
+
+    private void TransitionToDying()
+    {
+        ResetAllBools();
+        _animator.SetBool("Dying", true);
+    }
+
+    private void TransitionToDead()
+    {
+        ResetAllBools();
+        _animator.SetBool("Dead", true);
+    }
+
+    private void ResetAllBools()
+    {
+        _animator.SetBool("Idle", false);
+        _animator.SetBool("Reaching", false);
+        _animator.SetBool("Eating", false);
+        _animator.SetBool("Dying", false);
+        _animator.SetBool("Dead", false);
     }
 }
