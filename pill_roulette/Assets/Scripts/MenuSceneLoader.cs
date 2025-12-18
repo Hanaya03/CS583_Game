19fa9c45a1cbc5bad6efa562ec4ec65e9ec36313
Hanaya03 <ha4863519@gmail.com>
2025-12-17 18:23:09 -0800

    pressing retry loads you into correct scene

--- pill_roulette/Assets/Scripts/MenuSceneLoader.cs
+++ pill_roulette/Assets/Scripts/MenuSceneLoader.cs
@@ -4,10 +4,25 @@ using UnityEngine.SceneManagement;
 public class MenuSceneLoader : MonoBehaviour
 {
     [SerializeField] private string sceneName = "EnemyScene";
+    [SerializeField] private string sceneNameHard = "EnemySceneHard";
+
+    public void SelectEasyScene()
+    {
+        PlayerPrefs.SetString("SelectedDifficulty", sceneName);
+        PlayerPrefs.Save();
+        LoadScene();
+    }
+
+    public void SelectHardScene()
+    {
+        PlayerPrefs.SetString("SelectedDifficulty", sceneNameHard);
+        PlayerPrefs.Save();
+        LoadScene();
+    }
 
     public void LoadScene()
     {
-        SceneManager.LoadScene(sceneName);
+        SceneManager.LoadScene(PlayerPrefs.GetString("SelectedDifficulty"));
     }
 
     public void QuitGame()
